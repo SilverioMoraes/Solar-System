59a77659bdacd59e38f39405236d6c1c9383a689
Silverio Moraes <silverstone191013@gmail.com>
2022-06-21 14:56:41 -0300

    requesito-1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './App.css';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
     return (
-      <p>Sistema Solar</p>
+      <div className='App'>
+        <Header />
+      </div>
     );
   }
 }
